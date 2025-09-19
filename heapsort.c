fa6eebae5144bbce151c44e8ea436cca32def51e
Castillo Ramírez Constanza <cons.cr26@gmail.com>
2025-09-19 08:25:25 -0600

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -17,27 +17,6 @@ void max_heapify(int a[], int n, int i);
 void build_max_heap(int a[], int n);
 void heap_sort(int a[], int n);
 
-/*FUNCION PARA MANTENER LA PROPIEDAD DE MAX-HEAP */
-void max_heapify(int a[], inr n, int i) {
-    int largest = i; //Incializamos el mayor como la raiz 
-    int left = 2 * i +1; // Hijo izquierdo 
-    int right = 2 + i +2; //Hijo derecho
-
-    //Si el hijo izquierdo es mas grande que la raíz
-    if(left < n && a[left] > a[largest])
-        largest = right;
-
-    //Si el hijo derecho es más grande que el màs grande hasta ahora
-    if (largest != i) {
-        int temp = a[i];
-        a[i] = a[largest];
-        a[largest] temp;
-
-        //Recursivamente aplicamos max_heapify
-        max_heapify(a, n, largest);
-    }
-}
-
 /* MAIN de prueba mínima */
 int main(void) {
     int n; if (scanf("%d", &n) != 1 || n <= 0) return 0;
@@ -58,9 +37,25 @@ int main(void) {
     return 0;
 }
 
-/* IMPLEMENTAR AQUÍ */
-void max_heapify(int a[], int n, int i) {
-    /* TODO */
+//IMPLEMENTAR AQUI 
+void max_heapify(int a[], inr n, int i) {
+    int largest = i; //Incializamos el mayor como la raiz 
+    int left = 2 * i +1; // Hijo izquierdo 
+    int right = 2 + i +2; //Hijo derecho
+
+    //Si el hijo izquierdo es mas grande que la raíz
+    if(left < n && a[left] > a[largest])
+        largest = right;
+
+    //Si el hijo derecho es más grande que el màs grande hasta ahora
+    if (largest != i) {
+        int temp = a[i];
+        a[i] = a[largest];
+        a[largest] temp;
+
+        //Recursivamente aplicamos max_heapify
+        max_heapify(a, n, largest);
+    }
 }
 
 void build_max_heap(int a[], int n) {
